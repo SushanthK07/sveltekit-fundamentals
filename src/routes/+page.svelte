318bcd535804a78aaa58289e91ca40c7711a2265
SushanthK07 <sushanthkummitha@gmail.com>
2024-03-13 02:14:49 +0530

    navigate programatically

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,20 @@
+<script>
+	import { goto, beforeNavigate, afterNavigate } from '$app/navigation';
+
+	const handleOrder = () => {
+		goto('/products');
+		// goto('/products', { replaceState: true });
+	};
+
+	beforeNavigate((navigation) => {
+		console.log('Before navigate', navigation);
+	});
+
+	afterNavigate((navigation) => {
+		console.log('After navigate', navigation);
+	});
+</script>
+
 <h1>Home page</h1>
 
 <a href="/about">About</a>
@@ -6,3 +23,5 @@
 <a href="/marketing">Marketing</a>
 <a href="/profile">Profile</a>
 <a href="/products">Products</a>
+
+<button on:click={handleOrder}>Place Order</button>
