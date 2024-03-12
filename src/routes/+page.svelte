d47f33ca6fd39764e12e9e8683e161169c6a3801
SushanthK07 <sushanthkummitha@gmail.com>
2024-03-13 02:02:26 +0530

    routes navigation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,8 @@
 <h1>Home page</h1>
+
+<a href="/about">About</a>
+<a href="/blog">Blog</a>
+<a href="/docs">Docs</a>
+<a href="/marketing">Marketing</a>
+<a href="/profile">Profile</a>
+<a href="/products">Products</a>
--- src/routes/products/+page.svelte
+++ src/routes/products/+page.svelte
@@ -1,4 +1,6 @@
+<a href="/">Home</a>
+
 <h1>List of Products</h1>
-<h2>Product 1</h2>
-<h2>Product 2</h2>
-<h2>Product 3</h2>
+<h2><a href="/products/1">Product 1</a></h2>
+<h2><a href="/products/2">Product 2</a></h2>
+<h2><a href="/products/3">Product 3</a></h2>
