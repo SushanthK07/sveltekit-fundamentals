a50dfe6471ea3d42050c1a310c01f4ce2ea5a526
SushanthK07 <sushanthkummitha@gmail.com>
2024-03-13 01:52:37 +0530

    optional parameters

---
+++ src/routes/marketing/[[lang]]/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { page } from '$app/stores';
+
+	const greet = {
+		en: 'Hello',
+		es: 'Hola',
+		fr: 'Bonjour',
+		hi: 'Namaste',
+		te: 'హలో'
+	};
+
+	const lang = $page.params.lang || 'en';
+	const message = greet[lang];
+</script>
+
+<h1>{message}</h1>
