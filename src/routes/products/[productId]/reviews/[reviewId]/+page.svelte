109a0549c8cf028f2aa7df1bdd75f282d557223b
SushanthK07 <sushanthkummitha@gmail.com>
2024-03-13 01:20:42 +0530

    nested dynamic routes

---
+++ src/routes/products/[productId]/reviews/[reviewId]/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { page } from '$app/stores';
+
+	const productId = $page.params.productId;
+	const reviewId = $page.params.reviewId;
+</script>
+
+<h1>Review {reviewId} of product {productId}</h1>
